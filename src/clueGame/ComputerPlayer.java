8ed67754e86b965f91ead75996ab8ae010f15021
Alex Belli <alexbelli@Alexs-MacBook-Pro-2.local>
2021-03-28 22:03:02 -0600

    Passing test for the createSuggestion test

--- src/clueGame/ComputerPlayer.java
+++ src/clueGame/ComputerPlayer.java
@@ -1,6 +1,7 @@
 package clueGame;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class ComputerPlayer extends Player{
 	
@@ -19,6 +20,49 @@ public class ComputerPlayer extends Player{
 	public Solution createSuggestion(Card room) {
 		Solution solution = new Solution();
 		solution.setTheRoom(room);
+		int seen = 0;
+		
+		Random rand = new Random(playerDeck.size());
+		int randNumber = 0;
+		ArrayList<Card> personDeck = new ArrayList<Card>();
+		ArrayList<Card> weaponDeck = new ArrayList<Card>();
+		//Loops through the deck and adds each specific card type to its respective card ArrayList
+		//As long as it hasn't already been seen
+		for (Card c: playerDeck) {
+			if (c.getType().equals(CardType.PERSON) == true) {
+				for (Card x: seenPeopleCards) {
+					if (x == c) {
+						seen += 1;
+					}
+				}
+				if (seen > 0) {
+					seen = 0;
+					continue;
+				}
+				else {
+					personDeck.add(c);
+				}
+			} else if (c.getType().equals(CardType.WEAPON) == true) {
+				for (Card y: seenWeaponCards) {
+					if (y == c) {
+						seen += 1;
+					}
+				}
+				if (seen > 0) {
+					seen = 0;
+					continue;
+				}
+				else {
+					weaponDeck.add(c);
+				}
+			}
+		}
+		//Creates a random number based on the size of the array list, then sets the solution to
+		//a random card from that list for each of the respective card types
+		randNumber = rand.nextInt(personDeck.size());
+		solution.setThePerson(personDeck.get(randNumber));
+		randNumber = rand.nextInt(weaponDeck.size());
+		solution.setTheWeapon(weaponDeck.get(randNumber));
 		return solution;
 	}
 	
